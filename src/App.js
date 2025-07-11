cb65c2224613a6783f64d84601d4ae25aa605625
hruthiknaidu1609 <kollahruthik2004@gmail.com>
2025-07-12 00:41:00 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Stats from "./pages/stats";
-import RedirectPage from "./pages/RedirectPage"; // ✅ new
+import RedirectPage from "./pages/RedirectPage"; 
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/stats" element={<Stats />} />
-        <Route path="/:shortcode" element={<RedirectPage />} />  {/* ✅ add this */}
+        <Route path="/:shortcode" element={<RedirectPage />} />
       </Routes>
     </Router>
   );
